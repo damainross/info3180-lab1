efc7fc3980a462ac6307baf233bc79f59d4910a1
Damain Ross <damain.ross@mymona.uwi.edu>
2019-02-02 04:57:34 +0000

    created my first Flask app

--- app.py
+++ app.py
@@ -27,7 +27,6 @@ def home():
 @app.route('/about') 
 def about():
     return render_template('about.html') 
- 
 if __name__ == '__main__':
     app.run()
 app.run(debug=True, host="0.0.0.0", port=8080)
